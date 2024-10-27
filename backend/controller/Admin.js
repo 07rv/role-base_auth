2e729ca600fda035c298db6680d04c606d200866
Rohit Verma <28vrohit@gmail.com>
2024-10-27 19:26:45 +0530

    .

--- backend/controller/Admin.js
+++ backend/controller/Admin.js
@@ -13,4 +13,31 @@ const Getuser = async (req, res) => {
   }
 };
 
-export { Getuser };
+const deleteUser = async (req, res) => {
+  try {
+    const userId = req.params.id;
+    const checkRole = await UserModel.findById(userId, role);
+
+    if (checkRole === "admin") {
+      return res
+        .status(409)
+        .json({ sucess: false, message: "cannot delete user" });
+    }
+    const user = await UserModel.findByIdAndDelete(userId);
+
+    if (!user) {
+      return res.status(404).json({ sucess: false, message: "user not found" });
+    }
+
+    res
+      .status(200)
+      .json({ sucess: true, message: "users deleted", user: user });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Interval Server error" });
+  }
+};
+
+export { Getuser, deleteUser };
--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { Getuser } from "../controller/Admin.js";
+import { Getuser, deleteUser } from "../controller/Admin.js";
 import { isAdmin } from "../middleware/verifyToken.js";
 
 const AdminRoutes = express.Router();
 
 AdminRoutes.get("/getuser", isAdmin, Getuser);
+AdminRoutes.get("/delete:/:id", isAdmin, deleteUser);
 
 export default AdminRoutes;
