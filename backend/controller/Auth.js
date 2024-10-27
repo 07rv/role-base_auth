c484ab23ff91d951d4b33d60a9c2b78983180c8e
Rohit Verma <28vrohit@gmail.com>
2024-10-27 18:16:28 +0530

    Add user model and save in DB

--- backend/controller/Auth.js
+++ backend/controller/Auth.js
@@ -1,7 +1,32 @@
+import UserModel from "../models/user.js";
+
 const register = async (req, res) => {
   try {
-    res.send("asdfghm");
-  } catch (error) {}
+    const { name, email, password } = req.body;
+
+    const existUser = await UserModel.findOne({ email });
+
+    if (existUser) {
+      return res
+        .status(401)
+        .json({ sucess: false, message: "User Already exit" });
+    }
+
+    const newuser = new UserModel({
+      name,
+      email,
+      password,
+    });
+
+    await newuser.save();
+
+    res.status(200).json({ sucess: true, user: newuser });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ sucess: false, message: "Interval Server error" });
+  }
 };
 
 export { register };
--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import AuthRoutes from "./routes/Auth.js";
+import DbCon from "./utlies/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-
+DbCon();
 app.use(express.json());
 app.use(cors());
 
