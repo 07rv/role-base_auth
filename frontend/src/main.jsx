f8c55f286088cc02ad6a114d52364604eb404c4b
Rohit Verma <28vrohit@gmail.com>
2024-10-31 09:46:10 +0530

    .

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import NotFound from "./NotFound";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <App />
+      </Layout>
+    ),
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/trip",
+    element: (
+      <Layout>
+        <Trip />
+      </Layout>
+    ),
+    errorElement: <NotFound />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
